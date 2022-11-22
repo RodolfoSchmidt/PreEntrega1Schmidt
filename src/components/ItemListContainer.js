49c36754d594ded31ec99e829a1ca8be45df2c7a
Rodolfo Schmidt <rodolfoschmidt9@gmail.com>
2022-11-22 00:59:54 -0300

    primer commit

---
+++ src/components/ItemListContainer.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const ItemListContainer = ({greeting}) => {
+  return (
+    <div className='text-mono text-4xl text-sky-700'>{greeting}</div>
+  )
+}
+
+export default ItemListContainer
